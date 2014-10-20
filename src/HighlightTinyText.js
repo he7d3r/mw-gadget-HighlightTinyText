7271b18107aeec61a2a8c39c2a1c6de744c22d15
Helder <he7d3r@gmail.com>
2014-10-20 10:16:37 -0200

    Initial commit

---
+++ src/HighlightTinyText.js
@@ -0,0 +1,26 @@
+/**
+ * Add a link to highligh tiny text
+ * @author: Helder (https://github.com/he7d3r)
+ * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
+ */
+( function ( mw, $ ) {
+	'use strict';
+	var minFontSize = 10;
+
+	function addHighlightLink() {
+		$( mw.util.addPortletLink(
+			'p-cactions',
+			'#',
+			'Highlight tiny text',
+			'ca-tiny-text',
+			'Hightlight elements whose font-size is too small'
+		) ).click( function ( e ) {
+			$( '*' ).filter( function () {
+				return parseInt( $( this ).css( 'font-size' ), 10 ) < minFontSize;
+			} ).css( 'background-color', 'red' );
+		} );
+	}
+
+	$( addHighlightLink );
+
+}( mediaWiki, jQuery ) );
